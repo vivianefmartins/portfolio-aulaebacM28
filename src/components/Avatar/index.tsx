93397ba456b6a6e68e1161f3cb48de162bfb9d61
Viviane Martins <viane.fmar@gmail.com>
2026-07-03 18:58:14 -0300

    estilizando a sidebar

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,5 @@
+import { FotoPerfil } from './styles'
+
+const Avatar = () => <FotoPerfil src="https://github.com/vivianefmartins.png" />
+
+export default Avatar
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,19 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/vivianefmartins.png" />
-    <Titulo fontSize={16}>Viviane Martins</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={16}>Viviane Martins</Titulo>
+      <Paragrafo tipo="secundario">vivianefmartins</Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Desenvolvedora Full Stack
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
